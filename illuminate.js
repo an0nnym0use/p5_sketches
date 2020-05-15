89561ba78bdcb5f756f451874e78b1e6cfeeaece
an0nnym0use <risso.dylan@gmail.com>
2020-05-15 15:12:42 +0200

    +illuminate.js

---
+++ illuminate.js
@@ -0,0 +1,38 @@
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+//vars
+// var mouseFactor = 1;
+// noiseSeed(round(random()*1E6));
+
+function setup() {
+    let cnv = createCanvas(windowWidth, windowHeight);
+    cnv.position(0, 0);
+    cnv.style('z-index','-1');
+    background(0);
+    noStroke();
+
+    frameRate(30)
+    createLoop({duration:8, gif:true})
+}
+
+function draw() {
+    clear();
+    background(0);
+    // rotate(radians(60));
+    for (x=-2*width;x<2*width;x+=70) {
+        for (y=-2*height;y<2*height;y+=70) {
+            //mouse proximity checks
+            var mouseDist = dist(x,y,mouseX,mouseY);
+            var mouseFactor = (400/mouseDist)**6
+            if (mouseFactor<0.4) {
+                mouseFactor = 0.4;
+            }
+
+            let sound = noise(x, y, x*y);
+            fill(255, sound*40*mouseFactor);
+            ellipse(x,y,sound*70);
+        }
+    }
+}
