398e5d2ee42115788e0580e601c1dea60a7fa060
an0nnym0use <risso.dylan@gmail.com>
2020-05-17 18:40:37 +0200

    + forest.js

--- forest.js
+++ forest.js
@@ -5,41 +5,72 @@ function windowResized() {
 var branchAngle = Math.PI*0.1; //Change joint angle slightly randomly
 var detail = 12; //Vary the resolution
 var lenDecay = 0.65;
-var thickness = 2;
+var thickness = 2.5;
 
 function setup() {
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.position(0, 0);
     cnv.style('z-index','-1');
     background(0);
-    col1 = color('#213A7C');
-    col2 = color('#FF8A30');
+    col1 = color('#FF8A30');
+    col2 = color('#213A7C');
 
     stroke(0);
-    strokeWeight(2);
-    // frameRate(30);
-    // createLoop({duration:7, gif:true});
+    strokeWeight(thickness);
+
+    frameRate(5);
+    createLoop({duration:5, gif:false});
 }
 
 function draw() {
     setGradient(col1, col2);
-    translate(width/2, height); //Origin is the middle-bottom now
 
+    //Horizon Hills
+    beginShape();
+    for (x=0;x<width;x++) {
+        var curveX = map(x, 0, width, 0, 7); //Last number is spikiness
+        var y = 0.5*height*noise(curveX);
+        vertex(x, y);
+    }
+    endShape();
+    beginShape();
+    for (x=0;x<width;x++) {
+        var curveX = map(x, 0, width, 0, 3);
+        var y = height/3+0.2*height*noise(curveX);
+        vertex(x, y);
+    }
+    endShape();
+
+    //Trees
+    translate(width/2, height); //Origin is the middle-bottom now
     for (x=-width/2;x<width;x+=400) {
         for (y=-height/2;y<height/2;y+=100) {
             resetMatrix();
-            translate(width/2+random(-300,300), height+random(-100,100));
-            randomise();
-            drawBranch(60, x, y);
+            translate(width/2+random(-300,300), height+random(100));
+            treeInit();
+            drawBranch(random(50,70), x, y);
         }
     }
+
+    //Sun
+    push();
+    resetMatrix();
+    fill('#FFFBC8');
+    noStroke();
+    let sunX = random(50,width-50);
+    let sunY = random(90,190);
+    ellipse(sunX, sunY, 150);
+    fill('#FFFBC880');
+    ellipse(sunX, sunY, 170);
+    pop();
+
     noLoop();
 }
 
-function randomise() {
+function treeInit() {
     branchAngle = Math.PI*0.1; //Change joint angle slightly randomly
-    detail = 12 + random(-2,2); //Vary the resolution
-    lenDecay = 0.75 + random(-0.1,0.1);
+    detail = 12; //Vary the resolution
+    lenDecay = 0.75;
 }
 
 function drawBranch(length, x, y) {
@@ -47,14 +78,14 @@ function drawBranch(length, x, y) {
     strokeWeight(thickness);
     line(0, 0, 0, -length);
     translate(0, -length); //Now it draws from the ends of the newest line/s
-    if (length>detail) { //Detail = minimum branch length (resolution)
+    if (length>detail+random(2)) { //Detail = minimum branch length (resolution)
         push();
-        rotate(branchAngle*random(0.7,1.2));
-        drawBranch(length*lenDecay, 0, 0); //Length decreases
+        rotate(branchAngle*random(0.7,1.3));
+        drawBranch(length*(lenDecay+random(0.1)), 0, 0); //Length decreases
         pop();
         push();
-        rotate(-branchAngle*random(0.8,1.2)); //Other side = negative angle
-        drawBranch(length*lenDecay, 0, 0);
+        rotate(-branchAngle*random(0.7,1.3)); //Other side = negative angle
+        drawBranch(length*(lenDecay+random(0.1)), 0, 0);
         pop();
     }
 }
@@ -62,7 +93,7 @@ function drawBranch(length, x, y) {
 function setGradient(col1, col2) {
     noFill();
     for(var y=0; y<height; y++) {
-        var inter = map(y-300, 0, height-300, 0, 1);
+        var inter = map(y, 0, height, 0, 1);
         var c = lerpColor(col1, col2, inter);
         stroke(c);
         line(0, y, width, y);
